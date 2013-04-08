669f17fd23f185912631fc0e092d2393f8392422
theRookie <theRookie@localhost>
2013-04-08 10:05:31 +0300

    Some progress is made.

--- GoogleCodeJam/2009/QualificationRound/ProblemA/AlienLanguage/Solution.cs
+++ GoogleCodeJam/2009/QualificationRound/ProblemA/AlienLanguage/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -12,7 +13,34 @@ namespace AlienLanguage
             //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-small-practice.in"))
             //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-large-practice.in"))
             {
-                using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-large-practice.out"))
+                string inputNumbers = inputFile.ReadLine();
+                string[] numbers = inputNumbers.Split(' ');
+                int numberOfLetters = Convert.ToInt16(numbers[0]);
+                int numberOfKnownWords = Convert.ToInt16(numbers[1]);
+                int numberOfTestCases = Convert.ToInt16(numbers[2]);
+
+                List<string> knownWords = new List<string>();
+                for (int i = 0; i < numberOfKnownWords; i++)
+                {
+                    knownWords.Add(inputFile.ReadLine());
+                }
+
+                List<List<string>> testCases = new List<List<string>>();
+                for (int i = 0; i < numberOfTestCases; i++)
+                {
+                    string currentTestCase = inputFile.ReadLine();
+                    int lengthOfTestCase = currentTestCase.Length;
+
+                    for (int j = 0; j < lengthOfTestCase; j++)
+                    {
+ 
+                    }
+
+                }
+
+                
+
+                using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-example-practice.out"))
                 {
                         //outputFile.WriteLine("Case #" + outputLineNumber++ + ": " + sum);
                 }
