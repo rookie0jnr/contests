2b7a15b0a2603da3cf1d0caf85a72b43715312f9
theRookie <rookie0jnr@gmail.com>
2013-04-08 09:52:37 +0300

    Added the "output" folder.

--- GoogleCodeJam/2009/QualificationRound/ProblemA/AlienLanguage/Solution.cs
+++ GoogleCodeJam/2009/QualificationRound/ProblemA/AlienLanguage/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 
 namespace AlienLanguage
@@ -7,6 +8,15 @@ namespace AlienLanguage
     {
         static void Main(string[] args)
         {
+            using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-example-practice.in"))
+            //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-small-practice.in"))
+            //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-large-practice.in"))
+            {
+                using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-large-practice.out"))
+                {
+                        //outputFile.WriteLine("Case #" + outputLineNumber++ + ": " + sum);
+                }
+            }
         }
     }
 }
