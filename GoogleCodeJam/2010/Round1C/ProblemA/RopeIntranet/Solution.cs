73766c3f4b17d5f0bb760fae70de3e510c304733
theRookie <theRookie@localhost>
2013-04-09 08:42:23 +0300

    Problem implemented.

--- GoogleCodeJam/2010/Round1C/ProblemA/RopeIntranet/Solution.cs
+++ GoogleCodeJam/2010/Round1C/ProblemA/RopeIntranet/Solution.cs
@@ -9,20 +9,52 @@ namespace RopeIntranet
     {
         static void Main(string[] args)
         {
+            List<int> results = new List<int>();
+            
             using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-example-practice.in"))
             //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-small-practice.in"))
             //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-large-practice.in"))
             {
+                int numberOfTestCases = Convert.ToInt32(inputFile.ReadLine());
+                for (int i = 0; i < numberOfTestCases; i++)
+                {
+                    int numberOfWires = Convert.ToInt32(inputFile.ReadLine());
+                    int[,] wires = new int[numberOfWires, 2];
+
+                    for (int j = 0; j < numberOfWires; j++)
+                    {
+                        string[] wireEndPoints = inputFile.ReadLine().Split(' ');
+                        wires[j, 0] = Convert.ToInt32(wireEndPoints[0]);
+                        wires[j, 1] = Convert.ToInt32(wireEndPoints[1]);
+                    }
+
+                    int numberOfIntersectionPoints = 0;
+                    for (int j = 0; j < (numberOfWires - 1); j++)
+                    {
+                        for (int k = j; k < numberOfWires; k++)
+                        {
+                            if ( ((wires[j,0] - wires[k,0]) * (wires[j,1] - wires[k,1])) < 0 )
+                            {
+                                numberOfIntersectionPoints++;
+                            }
+                        }
+                    }
+
+                    results.Add(numberOfIntersectionPoints);
+                }
             }
 
             using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-example-practice.out"))
             //using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-small-practice.out"))
             //using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-large-practice.out"))
             {
-                //Console.WriteLine(numberOfPossibleMatches);
-                //outputFile.WriteLine("Case #{0}: {1}", i + 1, numberOfPossibleMatches);
+                //Console.WriteLine(numberOfIntersectionPoints);
+                int i = 1;
+                foreach (int result in results)
+                {
+                    outputFile.WriteLine("Case #{0}: {1}", i++, result);
+                }
             }
-            
         }
     }
 }
