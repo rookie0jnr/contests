e3ce8e7911570ade8b0d4ae08afda8fe66d297be
theRookie <rookie0jnr@gmail.com>
2013-04-09 11:48:07 +0300

    Solution created and ready for solving ;-)

---
+++ GoogleCodeJam/2010/Round1B/ProblemA/FileFix-it/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FileFix_it
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-example-practice.in"))
+            //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-small-practice.in"))
+            //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-large-practice.in"))
+            {
+                
+            }
+
+            using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-example-practice.out"))
+            //using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-small-practice.out"))
+            //using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-large-practice.out"))
+            {
+                //Console.WriteLine(numberOfIntersectionPoints);
+                //outputFile.WriteLine("Case #{0}: {1}", i++, result);
+            }
+        }
+    }
+}
