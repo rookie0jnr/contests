7cc9723f9918b669f23a783cc63ad8c47adf424e
theRookie <theRookie@localhost>
2013-04-09 08:16:18 +0300

    Basic operations performed.

--- GoogleCodeJam/2010/Round1C/ProblemA/RopeIntranet/Solution.cs
+++ GoogleCodeJam/2010/Round1C/ProblemA/RopeIntranet/Solution.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace RopeIntranet
@@ -7,6 +9,20 @@ namespace RopeIntranet
     {
         static void Main(string[] args)
         {
+            using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-example-practice.in"))
+            //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-small-practice.in"))
+            //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-large-practice.in"))
+            {
+            }
+
+            using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-example-practice.out"))
+            //using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-small-practice.out"))
+            //using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-large-practice.out"))
+            {
+                //Console.WriteLine(numberOfPossibleMatches);
+                //outputFile.WriteLine("Case #{0}: {1}", i + 1, numberOfPossibleMatches);
+            }
+            
         }
     }
 }
