416004c059bbdb9a110206d894c2853cecb8f1a6
theRookie <rookie0jnr@gmail.com>
2014-01-03 08:53:35 +0200

    Solution created and ready for solving.

---
+++ GoogleCodeJam/2010 Africa/QualificationRound/ProblemB/ReverseWords/ReverseWords/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWords
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            List<int> results = new List<int>();
+
+            using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-example-practice.in"))
+            //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-small-practice.in"))
+            //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-large-practice.in"))
+            {
+                int numberOfTestCases = Convert.ToInt32(inputFile.ReadLine());
+
+                for (int l = 0; l < numberOfTestCases; l++)
+                {
+                    string[] numberOfDirectories = inputFile.ReadLine().Split(' ');
+                    int numberOfExistingDirectories = Convert.ToInt32(numberOfDirectories[0]);
+                    int numberOfDirectoriesToCreate = Convert.ToInt32(numberOfDirectories[1]);
+                }
+
+            }
+
+            using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-example-practice.out"))
+            //using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-small-practice.out"))
+            //using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-large-practice.out"))
+            {
+
+                int i = 1;
+                foreach (int result in results)
+                {
+                    outputFile.WriteLine("Case #{0}: {1}", i++, result);
+                }
+            }
+        }
+    }
+}
