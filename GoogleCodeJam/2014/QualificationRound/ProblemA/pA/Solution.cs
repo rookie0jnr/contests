2be6837192d1ec79cbf86d046839e56f07860404
theRookie <theRookie@localhost>
2014-04-14 08:22:57 +0300

    Solutions commit as submitted to GCJ.

--- GoogleCodeJam/2014/QualificationRound/ProblemA/pA/Solution.cs
+++ GoogleCodeJam/2014/QualificationRound/ProblemA/pA/Solution.cs
@@ -12,131 +12,63 @@ namespace pA
         static void Main(string[] args)
         {
             List<string> results = new List<string>();
-            char[,] targets = new char[10, 4];
 
-            using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-example-practice.in"))
-            //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-small-attempt0.in"))
+            //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-example-practice.in"))
+            using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-small-attempt1.in"))
             //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\A-large.in"))
             {
                 int numberOfTestCases = Convert.ToInt32(inputFile.ReadLine());
                 for (int i = 0; i < numberOfTestCases; i++)
                 {
-                    string line1 = inputFile.ReadLine();
-                    string line2 = inputFile.ReadLine();
-                    string line3 = inputFile.ReadLine();
-                    string line4 = inputFile.ReadLine();
-                    inputFile.ReadLine();
+                    int answer1 = Convert.ToInt32(inputFile.ReadLine());
+                    string[] rows1 = new string[4];
+                    rows1[0] = inputFile.ReadLine();
+                    rows1[1] = inputFile.ReadLine();
+                    rows1[2] = inputFile.ReadLine();
+                    rows1[3] = inputFile.ReadLine();
 
-                    targets = null;
-                    targets = new char[10, 4];
-
-                    targets[0, 0] = line1[0];
-                    targets[0, 1] = line1[1];
-                    targets[0, 2] = line1[2];
-                    targets[0, 3] = line1[3];
-
-                    targets[1, 0] = line2[0];
-                    targets[1, 1] = line2[1];
-                    targets[1, 2] = line2[2];
-                    targets[1, 3] = line2[3];
-
-                    targets[2, 0] = line3[0];
-                    targets[2, 1] = line3[1];
-                    targets[2, 2] = line3[2];
-                    targets[2, 3] = line3[3];
-
-                    targets[3, 0] = line4[0];
-                    targets[3, 1] = line4[1];
-                    targets[3, 2] = line4[2];
-                    targets[3, 3] = line4[3];
-
-                    targets[4, 0] = line1[0];
-                    targets[4, 1] = line2[0];
-                    targets[4, 2] = line3[0];
-                    targets[4, 3] = line4[0];
-
-                    targets[5, 0] = line1[1];
-                    targets[5, 1] = line2[1];
-                    targets[5, 2] = line3[1];
-                    targets[5, 3] = line4[1];
-
-                    targets[6, 0] = line1[2];
-                    targets[6, 1] = line2[2];
-                    targets[6, 2] = line3[2];
-                    targets[6, 3] = line4[2];
-
-                    targets[7, 0] = line1[3];
-                    targets[7, 1] = line2[3];
-                    targets[7, 2] = line3[3];
-                    targets[7, 3] = line4[3];
-
-                    targets[8, 0] = line1[0];
-                    targets[8, 1] = line2[1];
-                    targets[8, 2] = line3[2];
-                    targets[8, 3] = line4[3];
-
-                    targets[9, 0] = line1[3];
-                    targets[9, 1] = line2[2];
-                    targets[9, 2] = line3[1];
-                    targets[9, 3] = line4[0];
-
-
-                    bool isSolved = false;
-                    for (int j = 0; j < 10; j++)
+                    List<int> targetRow1Numbers = new List<int>();
+                    string[] targetRow1 = rows1[answer1 - 1].Split(' ');
+                    foreach (string number in targetRow1)
                     {
-                        int countOfX = 0;
-                        int countOfO = 0;
-                        int countOfT = 0;
-
-                        for (int k = 0; k < 4; k++)
-                        {
-                            char currentChar = targets[j, k];
-                            if ('X'.Equals(currentChar))
-                            {
-                                countOfX++;
-                            }
-                            else if ('O'.Equals(currentChar))
-                            {
-                                countOfO++;
-                            }
-                            else if ('T'.Equals(currentChar))
-                            {
-                                countOfT++;
-                            }
-                        }
-
-                        if ((4 == countOfX) || ((3 == countOfX) && (1 == countOfT)))
-                        {
-                            results.Add("X won");
-                            isSolved = true;
-                            break;
-                        }
-                        else if ((4 == countOfO) || ((3 == countOfO) && (1 == countOfT)))
-                        {
-                            results.Add("O won");
-                            isSolved = true;
-                            break;
-                        }
+                        targetRow1Numbers.Add(Convert.ToInt32(number));
                     }
 
-                    if (!isSolved)
-                    {
-                        bool isEnded = ((line1.Contains<char>('.')) || (line2.Contains<char>('.')) || (line3.Contains<char>('.')) || (line4.Contains<char>('.')));
 
-                        if (isEnded)
-                        {
-                            results.Add("Game has not completed");
-                        }
-                        else
-                        {
-                            results.Add("Draw");
-                        }
+                    int answer2 = Convert.ToInt32(inputFile.ReadLine());
+                    string[] rows2 = new string[4];
+                    rows2[0] = inputFile.ReadLine();
+                    rows2[1] = inputFile.ReadLine();
+                    rows2[2] = inputFile.ReadLine();
+                    rows2[3] = inputFile.ReadLine();
+
+                    List<int> targetRow2Numbers = new List<int>();
+                    string[] targetRow2 = rows2[answer2 - 1].Split(' ');
+                    foreach (string number in targetRow2)
+                    {
+                        targetRow2Numbers.Add(Convert.ToInt32(number));
                     }
+
+                    List<int> result = targetRow1Numbers.Intersect(targetRow2Numbers).ToList<int>();
+
+                    if (1 == result.Count)
+                    {
+                        results.Add(result.FirstOrDefault().ToString());
+                    }
+                    else if (result.Count > 1)
+                    {
+                        results.Add("Bad magician!");
+                    }
+                    else
+                    {
+                        results.Add("Volunteer cheated!");
+                    }
+
                 }
             }
 
-            using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-example-practice.out"))
-            //using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-small-attempt0.out"))
+            //using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-example-practice.out"))
+            using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-small-attempt1.out"))
             //using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\A-large.out"))
             {
                 //Console.WriteLine(numberOfIntersectionPoints);
