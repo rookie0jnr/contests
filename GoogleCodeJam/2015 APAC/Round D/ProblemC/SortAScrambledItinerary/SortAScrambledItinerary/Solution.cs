02fa580281074dc9acc8dba41bb059a9528a66be
? <rookie0jnr@gmail.com>
2014-12-19 12:18:55 +0200

    Problem "Sort a scrambled itinerary" solved.

--- GoogleCodeJam/2015 APAC/Round D/ProblemC/SortAScrambledItinerary/SortAScrambledItinerary/Solution.cs
+++ GoogleCodeJam/2015 APAC/Round D/ProblemC/SortAScrambledItinerary/SortAScrambledItinerary/Solution.cs
@@ -13,68 +13,61 @@ namespace SortAScrambledItinerary
         {
             List<string> results = new List<string>();
 
-            using (StreamReader inputFile = new StreamReader(@"..\..\inputs\C-example-practice.in"))
+            //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\C-example-practice.in"))
             //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\C-small-practice.in"))
-            //using (StreamReader inputFile = new StreamReader(@"..\..\inputs\C-large-practice.in"))
+            using (StreamReader inputFile = new StreamReader(@"..\..\inputs\C-large-practice.in"))
             {
                 int numberOfTestCases = Convert.ToInt32(inputFile.ReadLine());
 
                 for (int l = 0; l < numberOfTestCases; l++)
                 {
-                    int N = Convert.ToInt32(inputFile.ReadLine());
-
-                    string[] gBuses = inputFile.ReadLine().Split(' ');
-
-                    List<Tuple<int, int>> gBusesList = new List<Tuple<int, int>>();
-                    for (int i = 0; i < (2 * N); i = i + 2)
+                    int numberOfTickets = Convert.ToInt32(inputFile.ReadLine());
+                    
+                    Dictionary<string, string> flightTickets = new Dictionary<string, string>();
+                    for (int i = 0; i < (numberOfTickets * 2); i = i + 2)
                     {
-                        gBusesList.Add(new Tuple<int, int>(Convert.ToInt32(gBuses[i]), Convert.ToInt32(gBuses[i + 1])));
-                    }
-                    gBusesList.Sort((a, b) => a.Item1.CompareTo(b.Item1));
+                        string source = inputFile.ReadLine();
+                        string destination = inputFile.ReadLine();
 
-                    int P = Convert.ToInt32(inputFile.ReadLine());
-                    int[] sities = new int[P];
-
-                    for (int i = 0; i < P; i++)
-                    {
-                        sities[i] = Convert.ToInt32(inputFile.ReadLine());
+                        flightTickets.Add(source, destination);
                     }
 
-                    int[] resultArray = new int[P];
-                    for (int i = 0; i < P; i++)
+                    string ticketSource = string.Empty;
+                    foreach (var source in flightTickets.Keys)
                     {
-                        int gBusesCoverCityCount = 0;
-                        foreach (Tuple<int, int> bus in gBusesList)
+                        if (!flightTickets.Values.Contains(source))
                         {
-                            if (bus.Item1 > sities[i])
-                            {
-                                break;
-                            }
-                            else if ((bus.Item1 <= sities[i]) && (bus.Item2 >= sities[i]))
-                            {
-                                gBusesCoverCityCount++;
-                            }
+                            ticketSource = source;
+                            break;
                         }
-
-                        resultArray[i] = gBusesCoverCityCount;
                     }
+
+                    List<string> sources = flightTickets.Keys.ToList<string>();
+                    List<string> destinations = flightTickets.Values.ToList<string>();
 
                     string testCaseResult = string.Empty;
-                    int res = 0;
-                    for (; res < P - 1; res++)
+                    for (int i = 0; i < sources.Count; i++)
                     {
-                        testCaseResult += resultArray[res] + " ";
+                        if (sources[i] == ticketSource)
+                        {
+                            testCaseResult += sources[i] + "-";
+                            testCaseResult += destinations[i] + " ";
+                            ticketSource = destinations[i];
+                            sources.RemoveAt(i);
+                            destinations.RemoveAt(i);
+                            i = -1;
+                        }
                     }
-                    testCaseResult += resultArray[res];
-                    results.Add(testCaseResult);
 
-                    inputFile.ReadLine();
+                    testCaseResult = testCaseResult.Remove(testCaseResult.Length - 1);
+                    results.Add(testCaseResult);
+                    //inputFile.ReadLine();
                 }
             }
 
-            using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\C-example-practice.out"))
+            //using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\C-example-practice.out"))
             //using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\C-small-practice.out"))
-            //using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\C-large-practice.out"))
+            using (StreamWriter outputFile = new StreamWriter(@"..\..\outputs\C-large-practice.out"))
             {
 
                 int i = 1;
