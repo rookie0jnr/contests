163e7e18cd1c852ef0833bbef9d555a3b5233675
theRookie <rookie0jnr@gmail.com>
2020-04-07 03:13:23 -0700

    An attempt to solve the first Test set!

--- GoogleCodeJam/2020/QualificationRound/ProblemB/NestingDepth/Solution.cs
+++ GoogleCodeJam/2020/QualificationRound/ProblemB/NestingDepth/Solution.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace NestingDepth
 {
@@ -19,7 +20,46 @@ namespace NestingDepth
             for (int i = 0; i < testCases; i++)
             {
                 string currentTestResult = "Case #" + (i + 1) + ": ";
-                results.Add(currentTestResult + inputs[i]);
+                string currentTestCase = inputs[i];
+
+                bool isParentisisOpened = false;
+                StringBuilder result = new StringBuilder();
+                for (int j = 0; j < currentTestCase.Length; j++)
+                {
+                    if (currentTestCase[j] == '0')
+                    {
+                        if (isParentisisOpened)
+                        {
+                            result.Append(')');
+                            isParentisisOpened = false;
+                        }
+                        result.Append('0');
+                    }
+                    else if (currentTestCase[j] == '1')
+                    {
+                        if (!isParentisisOpened)
+                        {
+                            result.Append('(');
+                        }
+                        result.Append('1');
+                        isParentisisOpened = true;
+                    }
+                    else
+                    {
+                        if (isParentisisOpened)
+                        {
+                            result.Append(")");
+                            isParentisisOpened = false;
+                        }
+                        result.Append(currentTestCase[j]);
+                    }
+                }
+                if (isParentisisOpened)
+                {
+                    result.Append(')');
+                }
+
+                results.Add(currentTestResult + result.ToString());
             }
 
             for (int i = 0; i < testCases; i++)
@@ -27,6 +67,5 @@ namespace NestingDepth
                 Console.WriteLine(results[i]);
             }
         }
-
     }
 }
