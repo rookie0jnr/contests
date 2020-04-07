da5e09d44a33691f0c2c98c7679a39939305a12d
theRookie <rookie0jnr@gmail.com>
2020-04-07 03:33:18 -0700

    Problem B solved completely!

--- GoogleCodeJam/2020/QualificationRound/ProblemB/NestingDepth/Solution.cs
+++ GoogleCodeJam/2020/QualificationRound/ProblemB/NestingDepth/Solution.cs
@@ -22,19 +22,43 @@ namespace NestingDepth
                 string currentTestResult = "Case #" + (i + 1) + ": ";
                 string currentTestCase = inputs[i];
 
-                bool isParentisisOpened = false;
                 StringBuilder result = new StringBuilder();
                 for (int j = 0; j < currentTestCase.Length; j++)
                 {
+                    result.Append(ANumberWithAllnecessaryParentesis(
+                        Convert.ToInt32(Char.GetNumericValue(currentTestCase[j]))));
                 }
 
-                results.Add(currentTestResult + result.ToString());
+                var stringResult = result.ToString();
+                while (stringResult.Contains(")("))
+                {
+                    stringResult = stringResult.Replace(")(", string.Empty);
+                }
+
+                results.Add(currentTestResult + stringResult);
             }
 
             for (int i = 0; i < testCases; i++)
             {
                 Console.WriteLine(results[i]);
             }
+        }
+
+        static string ANumberWithAllnecessaryParentesis(int number)
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < number; i++)
+            {
+                result += '(';
+            }
+            result += number;
+            for (int i = 0; i < number; i++)
+            {
+                result += ')';
+            }
+
+            return result;
         }
     }
 }
