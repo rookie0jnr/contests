7c78fac7e3e373f72ac6d62e7eff76d97a21cd61
theRookie <rookie0jnr@gmail.com>
2020-04-07 01:00:49 -0700

    Initial version for Problem B added. At this point - it only echoes the input.

---
+++ GoogleCodeJam/2020/QualificationRound/ProblemB/NestingDepth/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NestingDepth
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            List<string> results = new List<string>();
+            List<string> inputs = new List<string>();
+
+            for (int i = 0; i < testCases; i++)
+            {
+                inputs.Add(Console.ReadLine());
+            }
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string currentTestResult = "Case #" + (i + 1) + ": ";
+                results.Add(currentTestResult + inputs[i]);
+            }
+
+            for (int i = 0; i < testCases; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+        }
+
+    }
+}
