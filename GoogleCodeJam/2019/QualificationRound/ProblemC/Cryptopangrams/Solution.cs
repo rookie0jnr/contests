2173926761748954b540f33838f4081769fb7e01
theRookie <rookie0jnr@gmail.com>
2020-04-14 23:40:24 -0700

    Initial project for Cryptoangrams added.

---
+++ GoogleCodeJam/2019/QualificationRound/ProblemC/Cryptopangrams/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cryptopangrams
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            List<string> results = new List<string>();
+            List<string> inputs = new List<string>();
+
+            // Getting all the input
+            for (int i = 0; i < testCases; i++)
+            {
+                inputs.Add(Console.ReadLine());
+            }
+
+            // Solve
+            for (int i = 0; i < testCases; i++)
+            {
+                string currentTestResult = "Case #" + (i + 1) + ": ";
+
+
+                results.Add(currentTestResult);
+            }
+
+            // Writing results
+            for (int i = 0; i < testCases; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+        }
+    }
+}
