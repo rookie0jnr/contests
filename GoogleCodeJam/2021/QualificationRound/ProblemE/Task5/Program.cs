6397eaf31435a165ee148dbc53dbab5019c9ff91
theRookie <rookie0jnr@gmail.com>
2021-03-26 09:08:52 +0200

    Scafolded projects for QualificationRound 2021 added.

---
+++ GoogleCodeJam/2021/QualificationRound/ProblemE/Task5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            List<string> results = new List<string>();
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string currentTestResult = "Case #" + (i + 1) + ": ";
+
+
+                //currentTestResult += switches;
+                results.Add(currentTestResult);
+            }
+
+
+            for (int i = 0; i < testCases; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+        }
+    }
+}
