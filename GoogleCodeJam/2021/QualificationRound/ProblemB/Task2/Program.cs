748579a71aa01054c92b80b6a38c86276df4cbba
theRookie <rookie0jnr@gmail.com>
2021-03-29 07:31:47 +0300

    My actual correct solution of ProblemB (Moons and Umbrellas) during the contest. Please note, that this solution do NOT solve the third Test Set.

--- GoogleCodeJam/2021/QualificationRound/ProblemB/Task2/Program.cs
+++ GoogleCodeJam/2021/QualificationRound/ProblemB/Task2/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace Task2
 {
@@ -14,8 +17,38 @@ namespace Task2
             {
                 string currentTestResult = "Case #" + (i + 1) + ": ";
 
+                string[] input = Console.ReadLine().Split(' ');
+                int CJcost = Convert.ToInt32(input[0]);
+                int JCcost = Convert.ToInt32(input[1]);
 
-                //currentTestResult += switches;
+                StringBuilder mural = new StringBuilder(input[2]);
+                string muralString = mural.ToString();
+                if (muralString.StartsWith('?'))
+                {
+                    char startChar = 'C';
+                    if (muralString.IndexOf('C') > -1 && muralString.IndexOf('J') > -1 && muralString.IndexOf('J') < muralString.IndexOf('C')
+                        || (muralString.IndexOf('C') == -1 && muralString.IndexOf('J') > -1))
+                    {
+                        startChar = 'J';
+                    }
+                    mural[0] = startChar;
+                }
+
+                int currentQMIndex = 1;
+                while (mural.ToString().Contains('?'))
+                {
+                    if (mural[currentQMIndex] == '?')
+                        mural[currentQMIndex] = mural[currentQMIndex - 1];
+                    currentQMIndex++;
+                }
+
+                int total = 0;
+                total += Regex.Matches(mural.ToString(), "CJ").Count() * CJcost;
+                total += Regex.Matches(mural.ToString(), "JC").Count() * JCcost;
+
+
+
+                currentTestResult += total;
                 results.Add(currentTestResult);
             }
 
