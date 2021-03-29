f6aba9c86249b9bed456cb895bfe13f3821050d0
theRookie <rookie0jnr@gmail.com>
2021-03-29 07:33:53 +0300

    My attempt to solve the ProblemA (Reversort) during the contest. It solves correctly the sample tests, but fails on actual tests, because it tries to solve it, using strings.

--- GoogleCodeJam/2021/QualificationRound/ProblemA/Task1/Program.cs
+++ GoogleCodeJam/2021/QualificationRound/ProblemA/Task1/Program.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Task1
 {
     class Program
     {
+        static StringBuilder list;
+        //static void Main(string[] args)
+        //{
+        //    list = new StringBuilder("1243");
+        //    Reverse(2, 3 - 2 +1);
+        //}
+
+        static void Reverse(int start, int length)
+        {
+            string original = list.ToString().Substring(start, length);
+            for (int i = start, j = original.Length - 1; i < length + start; i++, j--)
+            {
+                list[i] = original[j];
+            }
+            
+        }
+
         static void Main(string[] args)
         {
             int testCases = Convert.ToInt32(Console.ReadLine());
@@ -14,8 +33,27 @@ namespace Task1
             {
                 string currentTestResult = "Case #" + (i + 1) + ": ";
 
+                int N = Convert.ToInt32(Console.ReadLine());
+                list = new StringBuilder(Console.ReadLine().Replace(" ", string.Empty));
 
-                //currentTestResult += switches;
+                int cost = 0;
+                for (int j = 0;  j < list.Length - 1;  j++)
+                {
+                    string listString = list.ToString();
+                    int minE = listString.IndexOf(listString.Substring(j, listString.Length - j).Min());
+
+                    if(minE == j)
+                    {
+                        cost++;
+                        continue;
+                    }
+
+                    cost += minE - j + 1;
+                    Reverse(j, minE - j + 1);
+                    
+                }
+
+                currentTestResult += cost;
                 results.Add(currentTestResult);
             }
 
