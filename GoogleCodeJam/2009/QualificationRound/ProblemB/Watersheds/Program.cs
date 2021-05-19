d15a82f7924926da0a1413f63a8b9e0d66729e74
theRookie <rookie0jnr@gmail.com>
2021-05-19 11:46:14 +0300

    ProblemB for 2009 project added.

---
+++ GoogleCodeJam/2009/QualificationRound/ProblemB/Watersheds/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Watersheds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            List<string> results = new List<string>();
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string currentTestResult = "Case #" + (i + 1) + ": ";
+
+                currentTestResult += " ";
+
+                results.Add(currentTestResult);
+            }
+
+            for (int i = 0; i < testCases; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+        }
+    }
+}
