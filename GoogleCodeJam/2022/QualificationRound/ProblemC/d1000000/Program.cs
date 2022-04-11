855778debb6ec2922c67520b2d9bf0983053dd6b
theRookie <rookie0jnr@gmail.com>
2022-04-11 08:39:38 +0300

    Problem solved.

--- GoogleCodeJam/2022/QualificationRound/ProblemC/d1000000/Program.cs
+++ GoogleCodeJam/2022/QualificationRound/ProblemC/d1000000/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace d1000000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            List<string> results = new List<string>();
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string currentTestResult = $"Case #{(i + 1)}: ";
+
+                Console.ReadLine(); // Just read (and discard) the N supplied
+                var inputNumbers = Console.ReadLine().Split(' ');
+                List<int> dices = new List<int>();
+                for (int j = 0; j < inputNumbers.Length; j++)
+                {
+                    dices.Add(Convert.ToInt32(inputNumbers[j]));
+                }
+
+                int straight = 0;
+                dices.Sort();
+                for (int j = 0; j < dices.Count; j++)
+                {
+                    if (straight < dices[j])
+                    {
+                        straight++;
+                    }
+                }
+
+                currentTestResult += straight;
+                results.Add(currentTestResult);
+            }
+
+
+            for (int i = 0; i < testCases; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+        }
+    }
+}
