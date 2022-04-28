dc9bd6d3a935461b6ab923eaf2466a143753bd65
theRookie <rookie0jnr@gmail.com>
2022-04-28 13:13:32 +0300

    Problem solved completely including the biggest test set!

--- GoogleCodeJam/2022/QualificationRound/ProblemD/ChainReactions/Program.cs
+++ GoogleCodeJam/2022/QualificationRound/ProblemD/ChainReactions/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace ChainReactions
 {
     class Program
     {
+        static Node[] nodes;
+
         class Node
         {
-            int next;
-            int FF;
-            bool isProcessed;
-            List<int> prevs = new List<int>();
+            internal int next;
+            internal int FF;
+            internal bool isProcessed;
+            internal List<int> prevs = new List<int>();
 
             public Node(int fF, int next)
             {
@@ -21,6 +24,7 @@ namespace ChainReactions
 
         static void Main(string[] args)
         {
+            //var p = Console.ReadLine();
             int testCases = Convert.ToInt32(Console.ReadLine());
             List<string> results = new List<string>();
 
@@ -32,14 +36,68 @@ namespace ChainReactions
                 var fFactors = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
                 var pointers = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
 
-                Node[] nodes = new Node[N];
+                nodes = new Node[N];
 
                 for (int j = 0; j < N; j++)
                 {
                     nodes[j] = new Node(fFactors[j], pointers[j]);
                 }
+                for (int j = 0; j < N; j++)
+                {
+                    if (nodes[j].next > -1)
+                    {
+                        nodes[nodes[j].next].prevs.Add(j);
+                    }
+                }
 
-                //currentTestResult += straight;
+                Stack<int> crossNodes = new Stack<int>();
+                for (int j = 0; j < N; j++)
+                {
+                    if (nodes[j].prevs.Count > 1)
+                    {
+                        crossNodes.Push(j);
+                    }
+                }
+
+                // start processing
+                long totalFun = 0;
+                while (crossNodes.Count > 0)
+                {
+                    var cross = nodes[crossNodes.Pop()];
+                    List<long> maxes = new List<long>();
+
+                    while (cross.prevs.Count > 0)
+                    {
+                        int branchMax = GoBack(nodes[cross.prevs[0]]);
+                        maxes.Add(branchMax);
+                        cross.prevs.RemoveAt(0);
+                    }
+
+                    int branchMin = (int)maxes[0];
+                    int indexMin = 0;
+                    for (int k = 0; k < maxes.Count; k++)
+                    {
+                        if (maxes[k] < branchMin)
+                        {
+                            indexMin = k;
+                            branchMin = (int)maxes[k];
+                        }
+                    }
+
+                    cross.FF = Math.Max(branchMin, cross.FF);
+                    maxes.RemoveAt(indexMin);
+                    totalFun += maxes.Sum();
+                }
+
+                for (int j = nodes.Length - 1; j >= 0; j--)
+                {
+                    if (!nodes[j].isProcessed)
+                    {
+                        totalFun += GoForward(nodes[j]);
+                    }
+                }
+
+                currentTestResult += totalFun;
                 results.Add(currentTestResult);
             }
 
@@ -49,5 +107,38 @@ namespace ChainReactions
                 Console.WriteLine(results[i]);
             }
         }
+
+        private static int GoBack(Node node)
+        {
+            var currentNode = node;
+            int max = 0;
+            while (currentNode.prevs.Count > 0)
+            {
+                max = Math.Max(max, currentNode.FF);
+                currentNode.isProcessed = true;
+                currentNode = nodes[currentNode.prevs[0]];
+            }
+            max = Math.Max(max, currentNode.FF);
+            currentNode.isProcessed = true;
+
+            return max;
+        }
+
+        private static int GoForward(Node node)
+        {
+            var currentNode = node;
+            int max = 0;
+            while (currentNode.next != -1)
+            {
+                max = Math.Max(max, currentNode.FF);
+                currentNode.isProcessed = true;
+                currentNode = nodes[currentNode.next];
+            }
+            max = Math.Max(max, currentNode.FF);
+            currentNode.isProcessed = true;
+
+            return max;
+        }
+
     }
 }
