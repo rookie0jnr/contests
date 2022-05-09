ab411f4c4c92d88e9fdf91e42f27715f0a1efa67
theRookie <rookie0jnr@gmail.com>
2022-05-09 11:38:32 +0300

    Project for AppendSort added.

---
+++ GoogleCodeJam/2021/Round 1A/ProblemA/AppendSort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ChainReactions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            List<string> results = new List<string>();
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string currentTestResult = $"Case #{(i + 1)}: ";
+
+                int N = Convert.ToInt32(Console.ReadLine());
+                
+                //currentTestResult += totalFun;
+                results.Add(currentTestResult);
+            }
+
+
+            for (int i = 0; i < testCases; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+        }
+    }
+}
