ebab59f789c5bced738f43df5e9f60b86356aca5
theRookie <rookie0jnr@gmail.com>
2022-05-12 12:21:48 +0300

    Initial project for the problem created.

---
+++ GoogleCodeJam/2022/Round 1A/ProblemA/DoubleOrOneThing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace DoubleOrOneThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            List<string> results = new List<string>();
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string currentTestResult = $"Case #{i + 1}: ";
+
+                string S = Console.ReadLine();
+                string result = S;
+
+
+                currentTestResult += result;
+                results.Add(currentTestResult);
+            }
+
+
+            for (int i = 0; i < testCases; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+        }
+    }
+}
