4dd87690bd2bf8e0e8113144e66571b935b503fb
theRookie <rookie0jnr@gmail.com>
2022-05-13 11:56:22 +0300

    Initial project added.

---
+++ GoogleCodeJam/2022/Round 1A/ProblemB/EqualSum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace EqualSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            List<string> results = new List<string>();
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string currentTestResult = $"Case #{i + 1}: ";
+
+                //currentTestResult += result;
+                results.Add(currentTestResult);
+            }
+
+
+            for (int i = 0; i < testCases; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+        }
+    }
+}
