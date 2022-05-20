416335e2e410d6e04c7c3eb9402cbb740de21f73
theRookie <rookie0jnr@gmail.com>
2022-05-20 09:59:35 +0300

    Project created.

---
+++ GoogleCodeJam/2018/PracticeSession/ProblemA/NumberGuessing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace NumberGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            for (int tcIndex = 0; tcIndex < testCases; tcIndex++)
+            {
+                int N = Convert.ToInt32(Console.ReadLine());
+                string result = string.Empty;
+
+                Console.WriteLine(result.TrimEnd());
+            }
+        }
+    }
+}
