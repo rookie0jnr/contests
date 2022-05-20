b6c1214319e71d8b8c7d45893f48241fa851f897
theRookie <rookie0jnr@gmail.com>
2022-05-20 09:52:30 +0300

    My implementation is ready. But it doesn't work. :( It seems that generating of the numbers should be quite different. And/or I don't understand the problem properly.

--- GoogleCodeJam/2022/Round 1A/ProblemB/EqualSum/Program.cs
+++ GoogleCodeJam/2022/Round 1A/ProblemB/EqualSum/Program.cs
@@ -9,21 +9,70 @@ namespace EqualSum
     {
         static void Main(string[] args)
         {
+            Console.Error.WriteLine("ping");
             int testCases = Convert.ToInt32(Console.ReadLine());
-            List<string> results = new List<string>();
-
+            Console.Error.WriteLine(testCases);
             for (int i = 0; i < testCases; i++)
             {
-                string currentTestResult = $"Case #{i + 1}: ";
+                int N = Convert.ToInt32(Console.ReadLine());
+                Console.Error.WriteLine(N);
+                List<long> numbers = new List<long>();
+                string myNumbers = string.Empty;
+                for (int j = 1; j <= N; j++)
+                {
+                    myNumbers += j.ToString() + " ";
+                    numbers.Add(j);
+                }
+                Console.WriteLine(myNumbers.TrimEnd());
+                Console.Error.WriteLine(myNumbers.TrimEnd());
 
-                //currentTestResult += result;
-                results.Add(currentTestResult);
-            }
+                string[] judgeNumbers = Console.ReadLine().Split(' ');
+                foreach (var item in judgeNumbers)
+                {
+                    Console.Error.WriteLine(item);
+                }
+                for (int l = 0; l < N; l++)
+                {
+                    numbers.Add(Convert.ToInt64(judgeNumbers[l]));
+                }
 
+                numbers.Sort();
+                long remaining = numbers.Sum() / 2;
 
-            for (int i = 0; i < testCases; i++)
-            {
-                Console.WriteLine(results[i]);
+                Stack<(long, int)> currentList = new Stack<(long, int)>();
+                int indexPointer = numbers.Count - 1;
+
+                while (true)
+                {
+                    if (indexPointer == -1)
+                    {
+                        var element = currentList.Pop();
+                        indexPointer = element.Item2 - 1;
+                        remaining += element.Item1;
+                        continue;
+                    }
+
+                    if (numbers[indexPointer] == remaining)
+                    {
+                        currentList.Push((numbers[indexPointer], indexPointer));
+                        break;
+                    }
+                    if (numbers[indexPointer] < remaining)
+                    {
+                        currentList.Push((numbers[indexPointer], indexPointer));
+                        remaining -= numbers[indexPointer];
+                    }
+
+                    indexPointer--;
+                }
+
+                string result = string.Empty;
+                foreach (var item in currentList)
+                {
+                    result += item.Item1.ToString() + " ";
+                }
+                Console.WriteLine(result.TrimEnd());
+                Console.Error.WriteLine(result.TrimEnd());
             }
         }
     }
