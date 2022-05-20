1e4e184ee60124d5e3b77015a9bbe443be4d6263
theRookie <rookie0jnr@gmail.com>
2022-05-20 11:15:33 +0300

    Problem (first interactive) solved!

--- GoogleCodeJam/2018/PracticeSession/ProblemA/NumberGuessing/Program.cs
+++ GoogleCodeJam/2018/PracticeSession/ProblemA/NumberGuessing/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Numerics;
 
 namespace NumberGuessing
 {
@@ -12,10 +9,34 @@ namespace NumberGuessing
             int testCases = Convert.ToInt32(Console.ReadLine());
             for (int tcIndex = 0; tcIndex < testCases; tcIndex++)
             {
+                var ab = Console.ReadLine().Split(' ');
+                int A = Convert.ToInt32(ab[0]);
+                int B = Convert.ToInt32(ab[1]) + 1;
                 int N = Convert.ToInt32(Console.ReadLine());
-                string result = string.Empty;
 
-                Console.WriteLine(result.TrimEnd());
+                int GUESS = (B - A) / 2;
+                Console.WriteLine(GUESS);
+                string responce = Console.ReadLine();
+                
+                while (!responce.Equals("CORRECT"))
+                {
+                    if (responce.Equals("TOO_BIG"))
+                    {
+                        B = GUESS;
+                    }
+                    else if (responce.Equals("TOO_SMALL"))
+                    {
+                        A = GUESS;
+                    }
+                    else if (responce.Equals("WRONG_ANSWER"))
+                    {
+                        break;
+                    }
+
+                    GUESS = (B - A) / 2 + A;
+                    Console.WriteLine(GUESS);
+                    responce = Console.ReadLine();
+                }
             }
         }
     }
