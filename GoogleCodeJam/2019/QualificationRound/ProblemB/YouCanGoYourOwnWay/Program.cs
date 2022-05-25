85f10242bcf9980d4087800311b4015af34496e0
theRookie <rookie0jnr@gmail.com>
2022-05-25 13:55:05 +0300

    On the way to solve.

--- GoogleCodeJam/2019/QualificationRound/ProblemB/YouCanGoYourOwnWay/Program.cs
+++ GoogleCodeJam/2019/QualificationRound/ProblemB/YouCanGoYourOwnWay/Program.cs
@@ -12,11 +12,81 @@ namespace YouCanGoYourOwnWay
             int testCases = Convert.ToInt32(Console.ReadLine());
             List<string> results = new List<string>();
 
-            for (int i = 0; i < testCases; i++)
+            for (int tcIndex = 0; tcIndex < testCases; tcIndex++)
             {
-                string currentTestResult = $"Case #{i + 1}: ";
+                string currentTestResult = $"Case #{tcIndex + 1}: ";
 
+                int N = Convert.ToInt32(Console.ReadLine());
+                string LyndaPath = Console.ReadLine();
 
+                char[,] maze = new char[N, N];
+                for(int k = 0; k < N; k++)
+                {
+                    for (int l = 0; l < N; l++)
+                    {
+                        maze[k, l] = ' ';
+                    }
+                }
+
+                bool shouldReachLastColumn = false;
+                if (LyndaPath[LyndaPath.Length - 1] == 'E')
+                {
+                    shouldReachLastColumn = true;
+                }
+
+                int i = 0, j = 0, lp = 0;
+                while (lp < LyndaPath.Length)
+                {
+                    maze[i, j] = LyndaPath[lp];
+                    if (LyndaPath[lp] == 'E')
+                    {
+                        j++;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                    lp++;
+                }
+
+                for (int k = 0; k < N; k++)
+                {
+                    for (int l = 0; l < N; l++)
+                    {
+                        Console.Write(maze[k, l].ToString() + ' ');
+                    }
+                    Console.WriteLine();
+                }
+
+                string myWay = string.Empty;
+                i = 0;
+                j = 0;
+                lp = 0;
+                if (shouldReachLastColumn)
+                {
+                    while (j <= N - 1)
+                    {
+                        if (maze[i, j] == 'S')
+                        {
+                            maze[i, j] = 'R';
+                            myWay += 'E';
+                            j++;
+                        }
+                        else if (maze[i, j] == 'E')
+                        {
+                            maze[i, j] = 'D';
+                            myWay += 'S';
+                            i++;
+                        }
+                    }
+                    while (i <= N -1)
+                    {
+                        myWay += 'S';
+                        maze[i, j] = 'D';
+                        i++;
+                    }
+
+                }
 
                 //currentTestResult += counter;
                 results.Add(currentTestResult);
