d03a848fb22b1659da9af885e72ed47f5f495530
theRookie <rookie0jnr@gmail.com>
2022-05-25 11:32:08 +0300

    Initial project created.

---
+++ GoogleCodeJam/2019/QualificationRound/ProblemB/YouCanGoYourOwnWay/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace YouCanGoYourOwnWay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            List<string> results = new List<string>();
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string currentTestResult = $"Case #{i + 1}: ";
+
+
+
+                //currentTestResult += counter;
+                results.Add(currentTestResult);
+            }
+
+
+            for (int i = 0; i < testCases; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+        }
+    }
+}
