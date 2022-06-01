8f1099e2a35eaae48efe050a36b1f479f8e5c6f6
theRookie <rookie0jnr@gmail.com>
2022-06-01 10:41:10 +0300

    Project added.

---
+++ GoogleCodeJam/2018/PracticeSession/ProblemC/Steed2CruiseControl/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Steed2CruiseControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            List<string> results = new List<string>();
+
+            for (int tcIndex = 0; tcIndex < testCases; tcIndex++)
+            {
+                string currentTestResult = $"Case #{tcIndex + 1}: ";
+
+
+
+                //currentTestResult += myWay.ToString();
+                results.Add(currentTestResult);
+            }
+
+            for (int i = 0; i < testCases; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+        }
+    }
+}
