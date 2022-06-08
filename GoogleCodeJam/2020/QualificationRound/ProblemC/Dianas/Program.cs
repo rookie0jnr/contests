200b7daad03eb2f514fa168d9ff1da8476f0f372
theRookie <rookie0jnr@gmail.com>
2022-06-08 10:13:45 +0300

    Final refactoring!

--- GoogleCodeJam/2020/QualificationRound/ProblemC/Dianas/Program.cs
+++ GoogleCodeJam/2020/QualificationRound/ProblemC/Dianas/Program.cs
@@ -1,71 +1,60 @@
-﻿
-namespace Parenting
-
+﻿namespace Parenting
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-
 
     class Program
     {
         static void Main(string[] args)
         {
             int testCases = Convert.ToInt32(Console.ReadLine());
-            for (int t = 0; t < testCases; t++)
+            string[] results = new string[testCases];
+
+            for (int tc = 0; tc < testCases; tc++)
             {
                 int CameronSTime = 0;
                 int JamieSTime = 0;
-                List<Tuple<int, int, int>> listTuples = new List<Tuple<int, int, int>>();
+                List<(int startTime, int endTime, int originalIndex)> allActivities =
+                    new List<(int, int, int)>();
 
                 int activities = Convert.ToInt32(Console.ReadLine());
                 for (int i = 0; i < activities; i++)
                 {
-                    int[] line = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
-                    listTuples.Add(new Tuple<int, int, int>(line[0], line[1], i));
+                    int[] activity = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                    allActivities.Add((activity[0], activity[1], i));
                 }
-                listTuples.Sort();
-                string[] parents = new string[listTuples.Count];
-                for (int i = 0; i < listTuples.Count; i++)
+
+                allActivities.Sort();
+                char[] result = new char[allActivities.Count];
+                for (int i = 0; i < allActivities.Count; i++)
                 {
-                    if (CameronSTime > listTuples[i].Item1 && JamieSTime > listTuples[i].Item1)
+                    var currentActivity = allActivities[i];
+                    if (CameronSTime > currentActivity.startTime && JamieSTime > currentActivity.startTime)
                     {
-                        parents = new string[] { "IMPOSSIBLE" };
+                        result = "IMPOSSIBLE".ToCharArray();
                         break;
                     }
                     else
                     {
-                      //  if (CameronStartTime <= listTuples[i].Item1 || JamieStartTime <= listTuples[i].Item1)
+                        if (CameronSTime <= currentActivity.startTime)
                         {
-                            if (CameronSTime <= listTuples[i].Item1) // == Math.Min(CameronStartTime, JamieStartTime))
-                            {
-                                CameronSTime = listTuples[i].Item2;
-                                parents[listTuples[i].Item3] = "C";
-                            }
-                            else
-                            {
-                                JamieSTime = listTuples[i].Item2;
-                                parents[listTuples[i].Item3] = "J";
-                            }
+                            CameronSTime = currentActivity.endTime;
+                            result[currentActivity.originalIndex] = 'C';
+                        }
+                        else
+                        {
+                            JamieSTime = currentActivity.endTime;
+                            result[currentActivity.originalIndex] = 'J';
                         }
                     }
                 }
-                if (parents == null)
-                {
-                    Console.WriteLine("Case #" + (t + 1) + ": IMPOSSIBLE");
-                }
-                else
-                {
-                    Console.Write("Case #" + (t + 1) + ": ");
-                    for (int i = 0; i < parents.Length; i++)
-                    {
-                        Console.Write(parents[i]);
-                    }
-                    if (t < testCases - 1)
-                    {
-                        Console.WriteLine();
-                    }
-                }
+
+                results[tc] = $"Case #{tc + 1}: {new string(result)}";
+            }
+
+            foreach (var item in results)
+            {
+                Console.WriteLine(item);
             }
         }
     }
