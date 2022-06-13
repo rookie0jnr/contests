11db1bfd5f848d04bd82df9cd72659bfcf6adcb6
theRookie <rookie0jnr@gmail.com>
2022-06-13 12:07:29 +0300

    Added Diana's (better) solution.

---
+++ GoogleCodeJam/2020/QualificationRound/ProblemB/Dianas/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace NestingDepth
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            for (int t = 0; t < testCases; t++)
+            {
+                Console.Write("Case #" + (t + 1) + ": ");
+                string line = Console.ReadLine();
+                int[] numbers = new int[line.Length];
+                for (int i = 0; i < line.Length; i++)
+                {
+                    numbers[i] = Convert.ToInt32(line[i].ToString());
+                }
+                int parenthesses = 0;
+                for (int i = 0; i < line.Length; i++)
+                {
+                    while (parenthesses < numbers[i])
+                    {
+                        Console.Write("(");
+                        parenthesses++;
+                    }
+                    Console.Write(numbers[i]);
+                    while (i < numbers.Length - 1
+                        && parenthesses > numbers[i + 1]
+                        && numbers[i] > 0
+                        && numbers[i + 1] < numbers[i])
+                    {
+                        Console.Write(")");
+                        parenthesses--;
+                    }
+                }
+                while (parenthesses > 0)
+                {
+                    Console.Write(")");
+                    parenthesses--;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
