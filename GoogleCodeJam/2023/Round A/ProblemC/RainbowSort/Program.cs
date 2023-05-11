1c1ac85056b6011de9ad9e212a45b72a9bbcab70
theRookie <rookie0jnr@gmail.com>
2023-05-11 10:31:29 +0300

    Project scaffolded.

---
+++ GoogleCodeJam/2023/Round A/ProblemC/RainbowSort/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace RainbowSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            List<string> results = new List<string>();
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string currentTestResult = $"Case #{(i + 1)}: ";
+                var input = Console.ReadLine().Split(' ');
+            }
+
+            for (int i = 0; i < testCases; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+        }
+    }
+}
