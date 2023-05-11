642dcd9e9ff38a0b3a32987c8e669a984c703701
theRookie <rookie0jnr@gmail.com>
2023-05-11 10:50:42 +0300

    I got TLE error on the Test Set 2.

--- GoogleCodeJam/2023/Round A/ProblemC/RainbowSort/Program.cs
+++ GoogleCodeJam/2023/Round A/ProblemC/RainbowSort/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace RainbowSort
+﻿using System;
+using System.Collections.Generic;
+
+namespace RainbowSort
 {
     internal class Program
     {
@@ -10,7 +13,36 @@
             for (int i = 0; i < testCases; i++)
             {
                 string currentTestResult = $"Case #{(i + 1)}: ";
+                Console.ReadLine();
                 var input = Console.ReadLine().Split(' ');
+
+                HashSet<string> colors = new HashSet<string>();
+                string previousColor = input[0];
+                colors.Add(previousColor);
+                string currentSequence = previousColor;
+
+                for (int j = 1; j < input.Length; j++)
+                {
+                    string currentColor = input[j];
+                    if (!currentColor.Equals(previousColor))
+                    {
+                        if (colors.Contains(currentColor))
+                        {
+                            currentSequence = "IMPOSSIBLE";
+                            break;
+                        }
+                        else
+                        {
+                            colors.Add(currentColor);
+                            currentSequence += $" {currentColor}";
+                            previousColor = currentColor;
+                        }
+
+                    }
+                }
+
+                currentTestResult += currentSequence;
+                results.Add(currentTestResult);
             }
 
             for (int i = 0; i < testCases; i++)
